9655adbbc5e902c67909a44b448553a9be4f2fd8
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-28 12:10:03 +0200

    try postgres docker setup

--- tests/ci/setup.sh
+++ tests/ci/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# get postgres running
+if [[ $PLATFORM == "gcc_64" ]]; then
+	apt-get -qq install docker-compose
+fi
+
+sudo docker-compose -f ./tools/qdatasyncserver/docker-compose.yaml up -d
+
 # disable test on osx as workaround
 if [[ $PLATFORM == "clang_64" ]]; then
 	echo "SUBDIRS -= CachingDataStoreTest" >> ./tests/auto/datasync/datasync.pro
