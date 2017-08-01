074d65ad48acfb31ae87d0a469fad6287577bd30
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-08-01 17:04:26 +0200

    fixed docker setup?

--- tests/ci/setup.sh
+++ tests/ci/setup.sh
@@ -6,11 +6,9 @@ if [[ $PLATFORM == "gcc_64" ]]; then
 	sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m)"
 	sudo chmod +x /usr/local/bin/docker-compose
 
-	sudo docker-compose -f ./tests/ci/docker-compose.yaml up -d
-	#docker run -d --name datasync_example_postgres -e POSTGRES_USER=qtdatasync -e POSTGRES_PASSWORD=baum42 -e POSTGRES_DB=QtDataSync postgres:latest
-
+	sudo docker run -d --name datasync_postgres -e "POSTGRES_PASSWORD=baum42" postgres:latest
 	
-	postgresIp=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' datasync_example_postgres)
+	postgresIp=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' datasync_postgres)
 	echo host=$postgresIp >> ./tests/ci/server-setup.conf
 	echo 'SETUP_FILE=$$PWD/../../../ci/server-setup.conf' > ./tests/auto/datasync/WsRemoteConnectorTest/setup.pri
 fi
