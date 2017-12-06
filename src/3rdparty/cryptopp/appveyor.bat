479686ba1e142af007d5063b35510f625f9f69ff
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-12-06 21:20:05 +0100

    fix 7z

--- src/3rdparty/cryptopp/appveyor.bat
+++ src/3rdparty/cryptopp/appveyor.bat
@@ -13,4 +13,4 @@ if "%pkgName%" == "" (
 )
 
 powershell -Command "Invoke-WebRequest https://github.com/Skycoder42/ci-builds/releases/download/cryptopp_%version%/cryptopp_%version%_%pkgName%.zip -OutFile %temp%\cryptopp.zip" || exit /B 1
-7z x %temp%\cryptopp.zip -o .\src\3rdparty || exit /B 1
+7z x %temp%\cryptopp.zip -o.\src\3rdparty || exit /B 1
