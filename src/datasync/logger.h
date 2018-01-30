d0df4b62dd8c3f0d4f2c7ce108882e63909c2f56
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-01-30 17:03:59 +0100

    added logger doc

--- src/datasync/logger.h
+++ src/datasync/logger.h
@@ -16,9 +16,8 @@ class Q_DATASYNC_EXPORT Logger : public QObject
 	Q_OBJECT
 
 public:
-	//! Constructor, internally used by Defaults::createLogger
+	//! @private
 	explicit Logger(QByteArray subCategory, const QString &setupName, QObject *parent = nullptr);
-	//! Destructor
 	~Logger();
 
 	//! Returns the logging category this logger logs to
@@ -26,6 +25,7 @@ public:
 
 	//! report a fatal error to the datasync engine
 	Q_NORETURN void reportFatalError(const QString &error, const char *file, int line, const char *function);
+	//! @copydoc Logger::reportFatalError(const QString &, const char *, int, const char *)
 	Q_NORETURN void reportFatalError(const char *error, const char *file, int line, const char *function);
 
 private:
@@ -34,7 +34,9 @@ private:
 
 }
 
+//! @private Internal macro
 #define QT_DATASYNC_LOG_BASE QMessageLogger(QT_MESSAGELOG_FILE, QT_MESSAGELOG_LINE, QT_MESSAGELOG_FUNC)
+//! @private Internal macro
 #define QT_DATASYNC_LOG_CAT QTDATASYNC_LOG->loggingCategory()
 //! A define to log like with qDebug, but using a logger. QTDATASYNC_LOG must be defined as a pointer to a logger
 #define logDebug() QT_DATASYNC_LOG_BASE.debug(QT_DATASYNC_LOG_CAT)
