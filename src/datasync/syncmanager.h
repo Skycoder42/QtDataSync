96be0c4e31bafa4cea784e3258f83206d426f5f6
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-01-31 14:28:52 +0100

    added sync manager doc

--- src/datasync/syncmanager.h
+++ src/datasync/syncmanager.h
@@ -16,61 +16,87 @@ class SyncManagerPrivateReplica;
 namespace QtDataSync {
 
 class SyncManagerPrivateHolder;
+//! Manages the synchronization process and reports its state
 class Q_DATASYNC_EXPORT SyncManager : public QObject
 {
 	Q_OBJECT
 
+	//! Specifies whether synchronization is currently enabled or disabled
 	Q_PROPERTY(bool syncEnabled READ isSyncEnabled WRITE setSyncEnabled NOTIFY syncEnabledChanged)
+	//! Holds the current synchronization state
 	Q_PROPERTY(SyncState syncState READ syncState NOTIFY syncStateChanged)
+	//! Holds the progress of the current sync operation
 	Q_PROPERTY(qreal syncProgress READ syncProgress NOTIFY syncProgressChanged)
+	//! Holds a description of the last internal error
 	Q_PROPERTY(QString lastError READ lastError NOTIFY lastErrorChanged)
 
 public:
+	//! The possible states the sync engine can be in
 	enum SyncState {
-		Initializing,
-		Downloading,
-		Uploading,
-		Synchronized,
-		Error,
-		Disconnected
+		Initializing, //!< Initializing internal stuff and connecting to a remote
+		Downloading, //!< Downloading changes from the remote
+		Uploading, //!< Uploading changes to the remote
+		Synchronized, //!< All changes have been synchronized. The engine is idle
+		Error, //!< An internal error occured. Synchronization is paused until reconnect() is called
+		Disconnected //!< The remote is not available or sync has been disabled and the engine thus is disconnected
 	};
 	Q_ENUM(SyncState)
 
+	//! @copydoc AccountManager::AccountManager(QObject*)
 	explicit SyncManager(QObject *parent = nullptr);
+	//! @copydoc AccountManager::AccountManager(const QString &, QObject*)
 	explicit SyncManager(const QString &setupName, QObject *parent = nullptr);
+	//! @copydoc AccountManager::AccountManager(QRemoteObjectNode *, QObject*)
 	explicit SyncManager(QRemoteObjectNode *node, QObject *parent = nullptr);
 	~SyncManager();
 
+	//! @copybrief AccountManager::replica
 	Q_INVOKABLE QRemoteObjectReplica *replica() const;
 
+	//! @readAcFn{syncEnabled}
 	bool isSyncEnabled() const;
+	//! @readAcFn{syncState}
 	SyncState syncState() const;
+	//! @readAcFn{syncProgress}
 	qreal syncProgress() const;
+	//! @readAcFn{lastError}
 	QString lastError() const;
 
+	//! Performs an operation once all changes have been downloaded
 	void runOnDownloaded(const std::function<void(SyncState)> &resultFn, bool triggerSync = true);
+	//! Performs an operation once all changes have been synchronized (both directions)
 	void runOnSynchronized(const std::function<void(SyncState)> &resultFn, bool triggerSync = true);
 
 public Q_SLOTS:
+	//! @writeAcFn{syncEnabled}
 	void setSyncEnabled(bool syncEnabled);
 
+	//! Triggers a synchronization
 	void synchronize();
+	//! Tries to reconnect to the remote
 	void reconnect();
 
 Q_SIGNALS:
+	//! @notifyAcFn{syncEnabled}
 	void syncEnabledChanged(bool syncEnabled, QPrivateSignal);
+	//! @notifyAcFn{syncState}
 	void syncStateChanged(QtDataSync::SyncManager::SyncState syncState, QPrivateSignal);
+	//! @notifyAcFn{syncProgress}
 	void syncProgressChanged(qreal syncProgress, QPrivateSignal);
+	//! @notifyAcFn{lastError}
 	void lastErrorChanged(const QString &lastError, QPrivateSignal);
+
+protected:
+	//! @private
+	SyncManager(QObject *parent, void *);
+	//! @private
+	void initReplica(const QString &setupName);
+	//! @private
+	void initReplica(QRemoteObjectNode *node);
 
 private Q_SLOTS:
 	void onInit();
 	void onStateReached(const QUuid &id, SyncState state);
-
-protected:
-	SyncManager(QObject *parent, void *);
-	void initReplica(const QString &setupName);
-	void initReplica(QRemoteObjectNode *node);
 
 private:
 	QScopedPointer<SyncManagerPrivateHolder> d;
@@ -78,7 +104,9 @@ private:
 	void runImp(bool downloadOnly, bool triggerSync, const std::function<void(SyncState)> &resultFn);
 };
 
+//! Stream operator to stream into a QDataStream
 Q_DATASYNC_EXPORT QDataStream &operator<<(QDataStream &stream, const SyncManager::SyncState &state);
+//! Stream operator to stream out of a QDataStream
 Q_DATASYNC_EXPORT QDataStream &operator>>(QDataStream &stream, SyncManager::SyncState &state);
 
 }
