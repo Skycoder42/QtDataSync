5a27993528a4ab3cae9d03995893540bb36ae3e7
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-02-03 01:25:49 +0100

    fix ci setup

--- tests/ci/setup.sh
+++ tests/ci/setup.sh
@@ -13,4 +13,5 @@ if [[ $PLATFORM == "gcc_64" ]]; then
 	postgresIp=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' datasync_postgres)
 	echo host=$postgresIp >> ./tests/ci/qdsapp.conf
 	echo 'SETUP_FILE=$$PWD/../../../ci/qdsapp.conf' > ./tests/auto/datasync/TestAppServer/setup.pri
+	echo 'SETUP_FILE=$$PWD/../../../ci/qdsapp.conf' > ./tests/auto/datasync/IntegrationTest/setup.pri
 fi
