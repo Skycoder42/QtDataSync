b9824f0119d4dea2d944bd3a0ee88b0f9f32c161
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-02-07 11:23:34 +0100

    debug qdep on osx

--- tests/ci/setup.sh
+++ tests/ci/setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
+scriptdir=$(dirname $0)
+
 # get postgres running for ws test
 if [[ $PLATFORM == "gcc_64" ]]; then
-	echo "CONFIG += include_server_tests" >> .qmake.conf
-
-	sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.18.0/docker-compose-$(uname -s)-$(uname -m)"
-	sudo chmod +x /usr/local/bin/docker-compose
-
 	sudo docker run -d --rm --name datasync_postgres -e "POSTGRES_PASSWORD=baum42" postgres:latest
 
 	postgresIp=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' datasync_postgres)
 	echo host=$postgresIp >> ./tests/ci/qdsapp.conf
 	echo 'SETUP_FILE=$$PWD/../../../ci/qdsapp.conf' > ./tests/auto/datasync/TestAppServer/setup.pri
 	echo 'SETUP_FILE=$$PWD/../../../ci/qdsapp.conf' > ./tests/auto/datasync/IntegrationTest/setup.pri
+fi
+
+# debug test
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+	echo "QDEP_PATH = $(which qdep)" > /opt/qt/$QT_VER/$PLATFORM/mkspecs/features/qdep.prf
+	cat $scriptdir/qdep.prf >> /opt/qt/$QT_VER/$PLATFORM/mkspecs/features/qdep.prf
 fi
